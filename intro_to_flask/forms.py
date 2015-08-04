9a3bf2d876661d91269049609ec4321d3705cafc
Craig McLaren <craigh89@yahoo.com>
2015-08-04 23:25:44 +0000

    another one

--- intro_to_flask/forms.py
+++ intro_to_flask/forms.py
@@ -1,23 +1,8 @@
-from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError, PasswordField
-from models import db, Officer
-
-class SignupForm(Form):
-  firstname = TextField("First name",  [validators.Required("Please enter your first name.")])
-  lastname = TextField("Last name",  [validators.Required("Please enter your last name.")])
-  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
-  password = PasswordField('Password', [validators.Required("Please enter a password.")])
-  submit = SubmitField("Create Profile")
+from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField
  
-  def __init__(self, *args, **kwargs):
-    Form.__init__(self, *args, **kwargs)
- 
-  def validate(self):
-    if not Form.validate(self):
-      return False
-     
-    user = User.query.filter_by(email = self.email.data.lower()).first()
-    if user:
-      self.email.errors.append("That email is already taken")
-      return False
-    else:
-      return True
+class ContactForm(Form):
+  name = TextField("Name")
+  email = TextField("Email")
+  subject = TextField("Subject")
+  message = TextAreaField("Message")
+  submit = SubmitField("Send")
